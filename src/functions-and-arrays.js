5a413ae3e13764ff58da70427aecc06aaedfa323
Posido Vega <posido@jobscreenr.com>
2021-02-24 21:56:19 -0500

    soleved exercise 4

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -73,3 +73,55 @@ function sum(array) {
 }
 
 // exercise 4
+
+function averageNumbers(array) {
+
+    let totalNumbers = 0;
+
+    for(let i = 0; i < array.length; i++) {
+
+        totalNumbers += array[i];
+
+    }
+
+    if (totalNumbers / array.length) {
+
+        return (totalNumbers / array.length);
+
+    } else return null
+}
+
+//exercise 4 level 2
+
+function averageWordLength(array) {
+    let totalLengths = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        totalLengths += array[i].length
+    }
+
+    if (totalLengths / array.length) {
+        return (totalLengths / array.length)
+    } else return null
+}
+
+//exercise 4 BONUS
+
+// const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
+
+function avg(arr) {
+    let legnthOfAnything = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === true) legnthOfAnything += 1;
+        else if (arr[i] === false) legnthOfAnything +=0;
+        else if (typeof arr[i] === 'string') legnthOfAnything += arr[i].length;
+        else if (typeof arr[i] === 'number') legnthOfAnything += arr[i];
+    }
+
+    if (legnthOfAnything / arr.length) {
+        arrayAverage = legnthOfAnything / arr.length;
+        return parseFloat(arrayAverage.toFixed(2));
+    } else return null
+
+}
