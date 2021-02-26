284011aa8b4138474d5fb7cebc07bdfd9cbf18fb
Posido Vega <posido@jobscreenr.com>
2021-02-25 23:44:21 -0500

    soleved some of exercise 5

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,11 +1,8 @@
 // exercise 1
 
 function maxOfTwoNumbers(num1, num2) {
-    if (num1 > num2) {
-        return num1;
-    } else {
-        return num2;
-    }
+    if (num1 > num2) return num1;
+    else return num2;
 }
 
 // function maxOfTwoNumbers(num1, num2) {
@@ -14,19 +11,13 @@ function maxOfTwoNumbers(num1, num2) {
 
 // exercise 2
 
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
-
-function findLongestWord(words) {
+function findLongestWord(array) {
     let wordItem = '';
 
-    if (words.length === 0) {
-        return null;
-    }
+    if (array.length === 0) return null;
 
-    for(let i = 0; i < words.length; i ++) {
-        if (words[i].length > wordItem.length) {
-            wordItem = words[i];
-        } 
+    for(let i = 0; i < array.length; i ++) {
+        if (array[i].length > wordItem.length) wordItem = array[i];
     }
 
     return wordItem;
@@ -34,13 +25,11 @@ function findLongestWord(words) {
 
 // exercise 3
 
-const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-
-function sumNumbers(numbers) {
+function sumNumbers(array) {
     let sumAllNumbers = 0;
 
-    for(let i = 0; i < numbers.length; i++) {
-        sumAllNumbers += numbers[i];
+    for(let i = 0; i < array.length; i++) {
+        sumAllNumbers += array[i];
     }
 
     return sumAllNumbers;
@@ -48,25 +37,17 @@ function sumNumbers(numbers) {
 
 // exercise 3 BONUS!!!
 
-const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
-
 function sum(array) {
 
     let sumMixedArr = 0;
 
     for(let i = 0; i < array.length; i++) {
 
-        if (typeof array[i] === 'number') {
-            sumMixedArr += array[i];
-        } else if (array[i] === true) {
-            sumMixedArr += 1;
-        } else if (array[i] === false) {
-            sumMixedArr += 0;
-        } else if (typeof array[i] === 'string') {
-            sumMixedArr += array[i].length;
-        } else if (typeof array[i] === 'object') {
-            throw Error("Unsupported data type sir or ma'am");
-        }
+        if (typeof array[i] === 'number') sumMixedArr += array[i];
+        else if (array[i] === true) sumMixedArr += 1;
+        else if (array[i] === false) sumMixedArr += 0;
+        else if (typeof array[i] === 'string') sumMixedArr += array[i].length;
+        else if (typeof array[i] === 'object') throw Error("Unsupported data type sir or ma'am");
     }
 
     return sumMixedArr;
@@ -79,16 +60,11 @@ function averageNumbers(array) {
     let totalNumbers = 0;
 
     for(let i = 0; i < array.length; i++) {
-
         totalNumbers += array[i];
-
     }
 
-    if (totalNumbers / array.length) {
-
-        return (totalNumbers / array.length);
-
-    } else return null
+    if (totalNumbers / array.length) return (totalNumbers / array.length);
+    else return null;
 }
 
 //exercise 4 level 2
@@ -97,17 +73,14 @@ function averageWordLength(array) {
     let totalLengths = 0;
 
     for (let i = 0; i < array.length; i++) {
-        totalLengths += array[i].length
+        totalLengths += array[i].length;
     }
 
-    if (totalLengths / array.length) {
-        return (totalLengths / array.length)
-    } else return null
+    if (totalLengths / array.length) return (totalLengths / array.length)
+    else return null;
 }
 
 //exercise 4 BONUS
-
-// const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
 
 function avg(arr) {
     let legnthOfAnything = 0;
@@ -122,6 +95,25 @@ function avg(arr) {
     if (legnthOfAnything / arr.length) {
         arrayAverage = legnthOfAnything / arr.length;
         return parseFloat(arrayAverage.toFixed(2));
-    } else return null
+    } else return null;
+
+}
+
+// exercise 5
+
+let filteredArr = [];
+
+function uniquifyArray(array) {
+
+    for (let i = 0; i < array.length; i++) {
+        let currentItem = array[i];
+        let uniqueIndex = array.indexOf(currentItem);
+
+        if (uniqueIndex >= i) filteredArr.push(array[uniqueIndex]); // only push unique item
+    }
+
+    if (array == false) return null;
+
+    return filteredArr;
 
 }
